97c8daa352634c681054eba0272170fc8788deb0
Qi Wang <1061673783@qq.com>
2023-04-24 17:01:35 -0500

    Adjusted code, current error: Chen's 2d simulation MSE of NN smaller than MSE of DK and CK

--- chen_simulation/scratch_paper.R
+++ chen_simulation/scratch_paper.R
@@ -4,29 +4,61 @@ for (curr_index in 1:10) {
   train_y <- sim_y[train_index]
   test_coords <- sim_coords[-train_index,]
   test_y <- sim_y[-train_index]
+  x_tr <- cbind(train_coords, basis_fun_1[train_index,],
+                basis_fun_2[train_index,],basis_fun_3[train_index,])
+  x_te <- cbind(test_coords, basis_fun_1[-train_index,],
+                basis_fun_2[-train_index,],basis_fun_3[-train_index,]) 
+  x_tr <- array_reshape( as.matrix(x_tr), c(length(train_y), ncol(x_tr)))
+  x_te <- array_reshape( as.matrix(x_te), c(length(test_y), ncol(x_tr))) 
+  model_dk <- keras_model_sequential()
+  model_dk %>% 
+    layer_dense(units = 100, activation = 'relu', input_shape = c(ncol(x_tr)), kernel_initializer = 'he_uniform') %>% 
+    layer_dense(units = 100, activation = 'relu') %>% 
+    layer_dense(units = 100, activation = 'relu') %>% 
+    layer_dense(units = 1, activation = 'linear')
   
+  model_dk %>% compile(
+    loss = "mse",
+    optimizer = optimizer_adam(),
+    metrics = list("mse")
+  )
   
+  model_checkpoint <- callback_model_checkpoint(
+    filepath = "best_weights.h5",
+    save_best_only = TRUE,
+    monitor = "val_loss",
+    mode = "min",
+    verbose = 1
+  )
+  
+  mod_train_dk <- model_dk %>%
+    fit(x = x_tr, y = train_y, epochs = 200, batch_size = 64, 
+        callbacks = list(model_checkpoint), validation_data = list(x_te, test_y))
+  model_dk %>% load_model_weights_hdf5("best_weights.h5")
+  
+  dkrig_mean_all[-train_index] <- predict(model_dk, x_te)
+  mse_vec_dkrig[curr_index] <- evaluate(model_dk, x_te, test_y)[2]
+  
+}
+
+for (curr_index in 1:10) {
+  train_index <- which(train_all_index != curr_index)
+  train_coords <- sim_coords[train_index,]
+  train_y <- sim_y[train_index]
+  test_coords <- sim_coords[-train_index,]
+  test_y <- sim_y[-train_index]
   # dnn_mean_all 
-  
   x_tr <- array_reshape( as.matrix(train_coords), c(length(train_y), 2))
   x_te <- array_reshape( as.matrix(test_coords), c(length(test_y), 2)) 
-  
   y_tr <- train_y
   y_te <- test_y
-  
+
   
   model_dnn <- keras_model_sequential()
   model_dnn %>% 
     layer_dense(units = 100, activation = 'relu', input_shape = c(ncol(x_tr)), kernel_initializer = 'he_uniform') %>% 
-    layer_batch_normalization() %>% 
     layer_dense(units = 100, activation = 'relu') %>% 
-    layer_batch_normalization() %>%
     layer_dense(units = 100, activation = 'relu') %>%
-    layer_batch_normalization() %>%
-    layer_dense(units = 100, activation = 'relu') %>% 
-    layer_batch_normalization() %>%
-    layer_dense(units = 100, activation = 'relu') %>%
-    layer_batch_normalization() %>%
     layer_dense(units = 1, activation = 'linear')
   
   model_dnn %>% compile(
@@ -35,17 +67,19 @@ for (curr_index in 1:10) {
     metrics = list("mse")
   )
   
-  mod_train_dnn <- model_dnn %>%
-    fit(x = x_tr, y = y_tr, epochs = 200, batch_size = 64)
+  model_checkpoint <- callback_model_checkpoint(
+    filepath = "best_weights.h5",
+    save_best_only = TRUE,
+    monitor = "val_loss",
+    mode = "min",
+    verbose = 1
+  )
+  dnn_history <- model_dnn %>%
+    fit(x = x_tr, y = y_tr, epochs = 200, batch_size = 64, callbacks = list(model_checkpoint), validation_data = list(x_te, test_y))
+  model_dnn %>% load_model_weights_hdf5("best_weights.h5")
+  
   
   nn_mean_all[-train_index] <- predict(model_dnn, x_te)
   mse_vec_nn[curr_index] <- evaluate(model_dnn, x_te, y_te)[2]
-
+  
 }
-
-
-p_dnn <- 
-  ggplot() +
-  geom_raster(aes(x = sim_coords[,1], y = sim_coords[,2], fill = nn_mean_all)) +
-  scale_fill_viridis_c(name = "") + 
-  labs(x = "Longitude",  y = "Latitude")
