d39e5eeaade8069f3caa9a109aa91ac62d8cf44f
Qi Wang <1061673783@qq.com>
2023-04-25 13:38:10 -0500

    Still modifying code

--- chen_simulation/scratch_paper.R
+++ chen_simulation/scratch_paper.R
@@ -9,20 +9,21 @@ for (curr_index in 1:10) {
   x_te <- cbind(test_coords, basis_fun_1[-train_index,],
                 basis_fun_2[-train_index,],basis_fun_3[-train_index,]) 
   x_tr <- array_reshape( as.matrix(x_tr), c(length(train_y), ncol(x_tr)))
-  x_te <- array_reshape( as.matrix(x_te), c(length(test_y), ncol(x_tr))) 
+  x_te <- array_reshape( as.matrix(x_te), c(length(test_y), ncol(x_te))) 
+  
   model_dk <- keras_model_sequential()
   model_dk %>% 
     layer_dense(units = 100, activation = 'relu', input_shape = c(ncol(x_tr)), kernel_initializer = 'he_uniform') %>% 
     layer_dense(units = 100, activation = 'relu') %>% 
     layer_dense(units = 100, activation = 'relu') %>% 
+    layer_dense(units = 100, activation = 'relu') %>% 
+    layer_dense(units = 100, activation = 'relu') %>% 
     layer_dense(units = 1, activation = 'linear')
-  
   model_dk %>% compile(
     loss = "mse",
     optimizer = optimizer_adam(),
     metrics = list("mse")
   )
-  
   model_checkpoint <- callback_model_checkpoint(
     filepath = "C:/Users/10616/Desktop/temp/best_weights.h5",
     save_best_only = TRUE,
@@ -30,12 +31,10 @@ for (curr_index in 1:10) {
     mode = "min",
     verbose = 1
   )
-  
   mod_train_dk <- model_dk %>%
     fit(x = x_tr, y = train_y, epochs = 200, batch_size = 64, 
         callbacks = list(model_checkpoint), validation_data = list(x_te, test_y))
   model_dk %>% load_model_weights_hdf5("C:/Users/10616/Desktop/temp/best_weights.h5")
-  
   dkrig_mean_all[-train_index] <- predict(model_dk, x_te)
   mse_vec_dkrig[curr_index] <- evaluate(model_dk, x_te, test_y)[2]
   
@@ -53,7 +52,6 @@ for (curr_index in 1:10) {
   y_tr <- train_y
   y_te <- test_y
 
-  
   model_dnn <- keras_model_sequential()
   model_dnn %>% 
     layer_dense(units = 100, activation = 'relu', input_shape = c(ncol(x_tr)), kernel_initializer = 'he_uniform') %>% 
@@ -75,7 +73,8 @@ for (curr_index in 1:10) {
     verbose = 1
   )
   dnn_history <- model_dnn %>%
-    fit(x = x_tr, y = y_tr, epochs = 200, batch_size = 64, callbacks = list(model_checkpoint), validation_data = list(x_te, test_y))
+    fit(x = x_tr, y = y_tr, epochs = 200, batch_size = 64, 
+        callbacks = list(model_checkpoint), validation_data = list(x_te, test_y))
   model_dnn %>% load_model_weights_hdf5("C:/Users/10616/Desktop/temp/best_weights.h5")
   
   
