fcb7498fcb8f4e1e00273e0c253ad46cc06aab3b
Qi Wang <1061673783@qq.com>
2023-07-14 09:53:44 +0800

    add tables to the code

---
+++ Qi_simulation/scoring_tables.R
@@ -0,0 +1,13 @@
+score_tab <- 
+rbind(
+-apply(cbind(crps_inla_all,crps_dnn_all,crps_dk_all,crps_ck_all),2, mean)
+,
+apply(cbind(int_score_inla,int_score_dnn,int_score_dk,int_score_ck),2, mean)
+)
+
+rownames(score_tab) <- c("Negative CRPS","Interval Score")
+colnames(score_tab) <- c("INLA","Base DNN", "DK","CK")
+
+xtable::xtable(round(score_tab,2),aline = "c")
+
+
